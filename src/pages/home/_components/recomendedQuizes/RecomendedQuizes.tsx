0fd9b99212e8ee84afd31bf304a78c945f475010
holden <al1onggwp@gmail.com>
2025-12-21 23:39:34 +0500

    temp

--- src/pages/home/_components/recomendedQuizes/RecomendedQuizes.tsx
+++ src/pages/home/_components/recomendedQuizes/RecomendedQuizes.tsx
@@ -1,20 +1,21 @@
-import { api } from "@/shared/api/api.handlers";
-import { apiRoutes } from "@/shared/api/api.routes";
-import type { IQuizResponse } from "@/shared/types/types";
-import useSwr from "swr";
+// import { api } from "@/shared/api/api.handlers";
+// import { apiRoutes } from "@/shared/api/api.routes";
+// import type { IQuizResponse } from "@/shared/types/types";
+// import useSwr from "swr";
 
 export const RecomendedQuizes = () => {
+  return <></>
     // api
-  const swrParams = {
-    method: "get",
-    url: apiRoutes.quizes,
-  };
-  const { data: quizes, isLoading } = useSwr<{
-    data: IQuizResponse[];
-    total: number;
-  }>([swrParams, "public"], api.sendRequest);
-  // temp slice until backend ready
-  const slicedQuizes = quizes?.data.slice(0,4) ?? [];
+  // const swrParams = {
+  //   method: "get",
+  //   url: apiRoutes.quizes,
+  // };
+  // const { data: quizes, isLoading } = useSwr<{
+  //   data: IQuizResponse[];
+  //   total: number;
+  // }>([swrParams, "public"], api.sendRequest);
+  // // temp slice until backend ready
+  // const slicedQuizes = quizes?.data.slice(0,4) ?? [];
 
 //   return (
 //     <div className="flex flex-row flex-nowrap gap-6 overflow-x-auto no-scrollbar">
