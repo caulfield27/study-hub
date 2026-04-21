ef3378c2ee2b65b4cc7576e20fb43be8d991685f
caulfield27 <al1onggwp@gmail.com>
2026-04-21 14:28:16 +0500

    fix: search input issue

--- src/app/layout/_components/sidebar/_components/globalSearch/GlobalSearch.tsx
+++ src/app/layout/_components/sidebar/_components/globalSearch/GlobalSearch.tsx
@@ -14,7 +14,7 @@ export const GlobalSearch = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
   
   const handleSearch = () => {
-    if (window.innerWidth <= 930 && window.innerHeight > 640) {
+    if (window.innerWidth <= 930 && window.innerWidth > 640) {
       setIsSidebarHidden(true);
     }
 
