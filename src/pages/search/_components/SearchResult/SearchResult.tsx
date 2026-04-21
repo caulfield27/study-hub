6fec821a5c81a36ab9a258bce30f6d96153d6eae
caulfield27 <al1onggwp@gmail.com>
2026-04-21 15:25:17 +0500

    fix: minor layout issue

--- src/pages/search/_components/SearchResult/SearchResult.tsx
+++ src/pages/search/_components/SearchResult/SearchResult.tsx
@@ -14,7 +14,7 @@ export const SearchResult = ({ data, total, query }: Props) => {
   const quizes = getQuizes(quizesData);
   return (
     <div className="h-full">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="max-w-7xl mx-auto">
         <div className="mb-8">
           <h1 className="text-3xl font-bold text-neutral-100 mb-2">Результаты поиска</h1>
           <p className="text-neutral-300">
